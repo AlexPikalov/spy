053467c63b64a582901a605b4339404509641950
Alex Pikalov <alex.pikalov.khar@gmail.com>
2019-08-24 19:32:16 +0200

    fix: fix multiple arguments spies

--- src/lib.rs
+++ src/lib.rs
@@ -2,40 +2,62 @@
 //! as Jasmine and Sinon.js. It provides easy configurable spies
 //! with predefined behaviour.
 //!
+//! # Spy without arguments
+//!
+//! `spy!()` creates a pair of a spy function that receives no arguments
+//! and does nothing and corresponded `Spy` object.
+//!
 //! ```rust
-//! #[test]
-//! fn iterator_all_test() {
-//!    let integers = vec![0i32, 1i32, 2i32];
-//!    
-//!    // create spy function that returns true if provided
-//!    // argument is an even number
-//!    let (spy_fn, spy) = spy!(|n| n % 2 == 0);
+//! #[macro_use]
+//! extern crate spy;
 //!
-//!    // test call
-//!    let res = integers.iter().all(spy_fn);
+//! use spy::*;
 //!
-//!    // check Iterator::all result
-//!    assert!(!res, "should be false");
+//! fn main() {
+//!     let (spy_fn, spy) = spy!();
 //!
-//!    // take a snapshot of made calls
-//!    let snapshot = spy.snapshot();
+//!     assert_eq!(spy_fn(), ());
+//!     assert_eq!(spy_fn(), ());
 //!
-//!    // make assertions
-//!    assert!(snapshot.called(), "should be called");
-//!    assert!(
-//!        snapshot.called_with(&(&1i32)),
-//!        "should be called with 1i32 at least once"
-//!    );
-//!    assert!(
-//!        !snapshot.each_called_with(&(&1i32)),
-//!        "should be called with different arguments"
-//!    );
-//!    assert_eq!(snapshot.all_calls(), &vec![(&0i32), (&1i32)]);
-//!    assert_eq!(snapshot.first_call().expect("should be Some"), &(&0i32));
-//!    assert_eq!(snapshot.last_call().expect("should be Some"), &(&1i32));
-//!    assert_eq!(snapshot.nth_call(1).expect("should be Some"), &(&1i32));
-//!}
+//!     // take a snapshot
+//!     let snapshot = spy.snapshot();
+//!
+//!     // make assertions
+//!     assert!(
+//!         snapshot.called_with(()),
+//!         "should be called with () at least once"
+//!     );
+//!     assert!(
+//!         snapshot.each_called_with(()),
+//!         "should be called with () each time"
+//!     );
+//!     assert_eq!(snapshot.num_of_calls(), 2, "should be called 2 times");
+//!     assert_eq!(snapshot.all_calls(), &vec![(), ()]);
+//!     assert_eq!(snapshot.first_call().expect("should be Some"), &());
+//!     assert_eq!(snapshot.last_call().expect("should be Some"), &());
+//!     assert_eq!(snapshot.nth_call(1).expect("should be Some"), &());
+//! }
+//!
 //! ```
+//!
+//! # Spy with arguments
+//!
+//! If a spy function should receive arguments it can be created in one
+//! of following ways:
+//!
+//! * `spy!(|n, m|)` creates a spy function that can receive two arguments.
+//! Any number of arguments could be passed inside `||`.
+//! Types of these arguments will be inferred from a spy function usage.
+//! This function does nothing and returns `()`.
+//!
+//! * `spy!(|n, m| a + b)` creates a spy function that can receive two
+//! arguments followed by a function body but any number of arguments could be requested.
+//! This syntax is pretty much the same as Rust closures. Types of arguments
+//! and an output will be inferred from a spy function usage.
+//!
+//! * `spy!(|n: u8, m: u8| -> u8 { return n + m; })` creates spy function
+//! with type annotations both for arguments and for an output.
+//!
 
 use std::sync::mpsc::Receiver;
 
@@ -50,7 +72,10 @@ impl<Args: PartialEq> Spy<Args> {
     pub fn snapshot(&self) -> SpySnapshot<Args> {
         let mut calls: Vec<Args> = vec![];
         loop {
-            match self.calls_recv.recv() {
+            match self
+                .calls_recv
+                .recv_timeout(::std::time::Duration::from_millis(0))
+            {
                 Ok(args) => calls.push(args),
                 Err(_) => {
                     break;
@@ -79,9 +104,9 @@ impl<Args: PartialEq> SpySnapshot<Args> {
 
     /// It returns `true` if spy function was called at least once
     /// with provided arguments and `false` otherwise.
-    pub fn called_with(&self, args: &Args) -> bool {
+    pub fn called_with(&self, args: Args) -> bool {
         for call in &self.calls {
-            if args == call {
+            if &args == call {
                 return true;
             }
         }
@@ -90,9 +115,9 @@ impl<Args: PartialEq> SpySnapshot<Args> {
 
     /// It returns `true` if spy function was called with provided arguments each time
     /// it was called.
-    pub fn each_called_with(&self, args: &Args) -> bool {
+    pub fn each_called_with(&self, args: Args) -> bool {
         for call in &self.calls {
-            if args != call {
+            if &args != call {
                 return false;
             }
         }
@@ -162,12 +187,35 @@ macro_rules! spy {
             (spy_fn, Spy{ calls_recv: recv })
         }
     };
+    (|$arg:ident|) => {
+        {
+            let (s, recv) = ::std::sync::mpsc::channel();
+            let spy_fn = move |$arg| {
+                s.send($arg)
+                    .expect("Problem with call agregation: cannot send call arguments to channel");
+            };
+            (spy_fn, Spy{ calls_recv: recv })
+        }
+    };
     (|$($arg:ident),*|) => {
         {
             let (s, recv) = ::std::sync::mpsc::channel();
             let spy_fn = move |$($arg),*| {
-                s.send($($arg),*)
+                let args_tuple = ($($arg,) *);
+                s.send(args_tuple)
                     .expect("Problem with call agregation: cannot send call arguments to channel");
+            };
+            (spy_fn, Spy{ calls_recv: recv })
+        }
+    };
+    (|$arg:ident| $expression:expr) => {
+        {
+            let (s, recv) = ::std::sync::mpsc::channel();
+
+            let spy_fn = move |$arg| {
+                s.send($arg)
+                    .expect("Problem with call agregation: cannot send call arguments to channel");
+                $expression
             };
             (spy_fn, Spy{ calls_recv: recv })
         }
@@ -175,8 +223,23 @@ macro_rules! spy {
     (|$($arg:ident),*| $expression:expr) => {
         {
             let (s, recv) = ::std::sync::mpsc::channel();
+
             let spy_fn = move |$($arg),*| {
-                s.send($($arg),*)
+                let args_tuple = ($($arg,) *);
+                s.send(args_tuple)
+                    .expect("Problem with call agregation: cannot send call arguments to channel");
+                $expression
+            };
+            (spy_fn, Spy{ calls_recv: recv })
+        }
+    };
+    (|$($arg:ident : $arg_type:ty),*| $(-> $return_type:ty)* $expression:block) => {
+        {
+            let (s, recv) = ::std::sync::mpsc::channel();
+
+            let spy_fn = move |$($arg : $arg_type),*| $(-> $return_type)* {
+                let args_tuple = ($($arg,) *);
+                s.send(args_tuple)
                     .expect("Problem with call agregation: cannot send call arguments to channel");
                 $expression
             };
@@ -208,11 +271,11 @@ mod tests {
 
         assert!(snapshot.called(), "should be called");
         assert!(
-            snapshot.called_with(&()),
+            snapshot.called_with(()),
             "should be called with () at least once"
         );
         assert!(
-            snapshot.each_called_with(&()),
+            snapshot.each_called_with(()),
             "should be called with () each time"
         );
         assert_eq!(snapshot.num_of_calls(), 3, "should be called 3 times");
@@ -224,55 +287,134 @@ mod tests {
 
     #[test]
     fn create_spy_with_args_test() {
-        let integers = vec![0i32, 1i32, 2i32];
-
         let (spy_fn, spy) = spy!(|n|);
 
-        // test via Iterator::for_each
-        integers.iter().for_each(spy_fn);
+        spy_fn(1u8);
+        spy_fn(2u8);
+
         let snapshot = spy.snapshot();
 
         assert!(snapshot.called(), "should be called");
+
         assert!(
-            snapshot.called_with(&(&1i32)),
-            "should be called with &1i32 at least once"
+            snapshot.called_with(1u8),
+            "should be called with &1u8 at least once"
         );
         assert!(
-            !snapshot.each_called_with(&(&1i32)),
+            !snapshot.each_called_with(1u8),
             "should be called with different arguments"
         );
-        assert_eq!(snapshot.num_of_calls(), 3, "should be called 3 times");
-        assert_eq!(snapshot.all_calls(), &vec![(&0i32), (&1i32), (&2i32)]);
-        assert_eq!(snapshot.first_call().expect("should be Some"), &(&0i32));
-        assert_eq!(snapshot.last_call().expect("should be Some"), &(&2i32));
-        assert_eq!(snapshot.nth_call(1).expect("should be Some"), &(&1i32));
+        assert_eq!(snapshot.num_of_calls(), 2, "should be called 3 times");
+        assert_eq!(snapshot.all_calls(), &vec![1u8, 2u8]);
+        assert_eq!(snapshot.first_call().expect("should be Some"), &1u8);
+        assert_eq!(snapshot.last_call().expect("should be Some"), &2u8);
+        assert_eq!(snapshot.nth_call(1).expect("should be Some"), &2u8);
+    }
+
+    #[test]
+    fn create_spy_with_many_args_test() {
+        let (spy_fn, spy) = spy!(|n, s|);
+
+        spy_fn(1u8, "first");
+        spy_fn(2u8, "second");
+
+        let snapshot = spy.snapshot();
+
+        assert!(snapshot.called(), "should be called");
+
+        assert!(
+            snapshot.called_with((1u8, "first")),
+            "should be called with &1u8 at least once"
+        );
+        assert!(
+            !snapshot.each_called_with((1u8, "first")),
+            "should be called with different arguments"
+        );
+        assert_eq!(snapshot.num_of_calls(), 2, "should be called 3 times");
+        assert_eq!(snapshot.all_calls(), &vec![(1u8, "first"), (2u8, "second")]);
+        assert_eq!(
+            snapshot.first_call().expect("should be Some"),
+            &(1u8, "first")
+        );
+        assert_eq!(
+            snapshot.last_call().expect("should be Some"),
+            &(2u8, "second")
+        );
+        assert_eq!(
+            snapshot.nth_call(1).expect("should be Some"),
+            &(2u8, "second")
+        );
     }
 
     #[test]
     fn create_spy_with_args_and_return_test() {
-        let integers = vec![0i32, 1i32, 2i32];
-
         let (spy_fn, spy) = spy!(|n| n % 2 == 0);
 
-        // test via Iterator::all
-        let res = integers.iter().all(spy_fn);
-
-        assert!(!res, "should be false");
+        assert!(spy_fn(2), "should return true for an even number");
+        assert!(!spy_fn(3), "should return false for an odd number");
 
         let snapshot = spy.snapshot();
 
         assert!(snapshot.called(), "should be called");
         assert!(
-            snapshot.called_with(&(&1i32)),
-            "should be called with &1i32 at least once"
+            snapshot.called_with(2),
+            "should be called with 2 at least once"
         );
         assert!(
-            !snapshot.each_called_with(&(&1i32)),
+            !snapshot.each_called_with(2),
             "should be called with different arguments"
         );
-        assert_eq!(snapshot.all_calls(), &vec![(&0i32), (&1i32)]);
-        assert_eq!(snapshot.first_call().expect("should be Some"), &(&0i32));
-        assert_eq!(snapshot.last_call().expect("should be Some"), &(&1i32));
-        assert_eq!(snapshot.nth_call(1).expect("should be Some"), &(&1i32));
+        assert_eq!(snapshot.all_calls(), &vec![2, 3]);
+        assert_eq!(snapshot.first_call().expect("should be Some"), &2);
+        assert_eq!(snapshot.last_call().expect("should be Some"), &3);
+        assert_eq!(snapshot.nth_call(1).expect("should be Some"), &3);
+    }
+
+    #[test]
+    fn create_spy_with_many_args_and_return_test() {
+        let (spy_fn, spy) = spy!(|n, m| n + m);
+
+        assert_eq!(spy_fn(2u8, 1u8), 3u8, "should return 3");
+        assert_eq!(spy_fn(3u8, 1u8), 4u8, "should return 4");
+
+        let snapshot = spy.snapshot();
+
+        assert!(snapshot.called(), "should be called");
+        assert!(
+            snapshot.called_with((2u8, 1u8)),
+            "should be called with 2 at least once"
+        );
+        assert!(
+            !snapshot.each_called_with((2u8, 1u8)),
+            "should be called with different arguments"
+        );
+        assert_eq!(snapshot.all_calls(), &vec![(2, 1), (3, 1)]);
+        assert_eq!(snapshot.first_call().expect("should be Some"), &(2, 1));
+        assert_eq!(snapshot.last_call().expect("should be Some"), &(3, 1));
+        assert_eq!(snapshot.nth_call(1).expect("should be Some"), &(3, 1));
+    }
+
+    #[test]
+    fn create_spy_type_annotations() {
+        let (spy_fn, spy) = spy!(|n: u8, m: u8| -> u8 { n + m });
+
+        assert_eq!(spy_fn(2, 1), 3, "should return 3");
+        assert_eq!(spy_fn(3, 1), 4, "should return 4");
+
+        let snapshot = spy.snapshot();
+
+        assert!(snapshot.called(), "should be called");
+        assert!(
+            snapshot.called_with((2, 1)),
+            "should be called with 2 at least once"
+        );
+        assert!(
+            !snapshot.each_called_with((2, 1)),
+            "should be called with different arguments"
+        );
+        assert_eq!(snapshot.all_calls(), &vec![(2, 1), (3, 1)]);
+        assert_eq!(snapshot.first_call().expect("should be Some"), &(2, 1));
+        assert_eq!(snapshot.last_call().expect("should be Some"), &(3, 1));
+        assert_eq!(snapshot.nth_call(1).expect("should be Some"), &(3, 1));
     }
 }
